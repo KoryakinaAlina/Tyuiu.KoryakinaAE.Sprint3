afd281ffb41218a73d74b9e18b8c6f99586e4d96
KoryakinaAlina <Tyuiu.KoryakinaAE@outlook.com>
2025-10-31 09:06:24 +0500

    Task2

---
+++ Tyuiu.KoryakinaAE.Sprint3.Task2.V27/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.KoryakinaAE.Sprint3.Task2.V27.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнила: Корякина А.Э. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла do-while                                           *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #27                                                             *");
+        Console.WriteLine("* Выполнила: Корякина Алина Эльдаяговна | ИСТНб-25-1                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу используя цикл do...while,                           *");
+        Console.WriteLine("* которая вычисляет сумму ряда по формуле, при х=5.                       *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* (4/(1+x^i))^i                                                           *");
+        int x = 5;
+        int y = 14;
+        int i = 1;
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.GetSumSeries(x, i, y));
+        Console.ReadLine();
+    }
+}
